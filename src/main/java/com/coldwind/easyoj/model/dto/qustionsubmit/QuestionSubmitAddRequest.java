21d4b90b9361be153f6841ca01d758113de2ee00
libolin <3167967427@qq.com>
2025-05-10 12:16:24 +0800

    重新初始化仓库

---
+++ src/main/java/com/coldwind/easyoj/model/dto/qustionsubmit/QuestionSubmitAddRequest.java
@@ -0,0 +1,35 @@
+package com.coldwind.easyoj.model.dto.qustionsubmit;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 创建请求
+ *
+ * EL PSY KONGROO
+ */
+@Data
+public class QuestionSubmitAddRequest implements Serializable {
+    /**
+     * 编程语言
+     */
+    private String language;
+
+    /**
+     * 用户代码
+     */
+    private String code;
+
+    /**
+     * 题目 id
+     */
+    private Long questionId;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/coldwind/easyoj/model/entity/User.java
@@ -0,0 +1,105 @@
+package com.coldwind.easyoj.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 用户
+ *
+ * EL PSY KONGROO
+ */
+@TableName(value = "user")
+@Data
+public class User implements Serializable {
+
+    /**
+     * id
+     */
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 用户账号
+     */
+    private String userAccount;
+
+    /**
+     * 用户密码
+     */
+    private String userPassword;
+
+    /**
+     * 开放平台id
+     */
+    private String unionId;
+
+    /**
+     * 公众号openId
+     */
+    private String mpOpenId;
+
+    /**
+     * 用户昵称
+     */
+    private String userName;
+
+    /**
+     * 用户头像
+     */
+    private String userAvatar;
+
+    /**
+     * 用户简介
+     */
+    private String userProfile;
+
+    /**
+     * 用户角色：user/admin/ban
+     */
+    private String userRole;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    private Integer isDelete;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+
+    private String verificationCode;
+
+    private Date verificationCodeExpireTime;
+
+    public String getVerificationCode() {
+        return verificationCode;
+    }
+
+    public void setVerificationCode(String verificationCode) {
+        this.verificationCode = verificationCode;
+    }
+
+    public Date getVerificationCodeExpireTime() {
+        return verificationCodeExpireTime;
+    }
+
+    public void setVerificationCodeExpireTime(Date verificationCodeExpireTime) {
+        this.verificationCodeExpireTime = verificationCodeExpireTime;
+    }
+}
